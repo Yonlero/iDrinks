e72d20ddb00524f989f0201e3e25da9c5b221ea2
Yonlero <abraaobrg150@gmail.com>
2019-10-19 15:21:02 -0300

    Adicionei as Classes "Cliente" e "Endereco" no pacote Pessoa

---
+++ src/Pessoa/Cliente.java
@@ -0,0 +1,63 @@
+package Pessoa;
+
+public class Cliente{
+    //------------------------Atributos Comum para todos Cliente-----------------------------
+    private String Nome;
+    private String CPF;
+    private String Email;
+    private String Telefone;
+    private String DataNascimento;
+    private Endereco EnderecoPessoa;
+    //------------------------Começando os Metodos GET de todos esses atributos--------------
+
+    public String getNome() { // Retornando o Nome
+        return Nome;
+    }
+
+    public String getCPF() { // Retornando o CPF
+        return CPF;
+    }
+    
+    public String getEmail() { // Retornando o Email
+        return Email;
+    }
+    
+    public String getTelefone() { // Retornando o Telefone
+        return Telefone;
+    }
+
+    public String getDataNascimento() { // Retornando a Data de Nascimento
+        return DataNascimento;
+    }
+
+    public Endereco getEnderecoPessoa() { // Retornando o Endereco da Pessoa
+        return EnderecoPessoa;
+    }
+
+    //-----------------------Começando os metodos SET de todos esses atributos-----------------
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+    
+    public void setDataNascimento(String DataNascimento) {
+        this.DataNascimento = DataNascimento;
+    }
+
+    public void setEnderecoPessoa(Endereco EnderecoPessoa) {
+        this.EnderecoPessoa = EnderecoPessoa;
+    }
+    
+}
